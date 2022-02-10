2b6ee0f9d3472040320d20ffaa636d2bde32ff0e
alipbruh <99107279+alipbruh@users.noreply.github.com>
2022-02-10 15:19:07 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -7,7 +7,7 @@ global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 // setting
 global.ownername ="Alip"
 global.ownernumber = "6281914981500"
-global.botname = "Haruka-Bot"
+global.botname = "Test Bot"
 global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.limit = {
